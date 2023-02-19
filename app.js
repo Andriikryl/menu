48d29289b2e686f4c53501b04bc03ad2efcc4ef5
Amdrii <andreyk557@gmail.com>
2023-02-19 19:27:03 +0200

    feat[sub-menu-js]

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 document.querySelector('.root-nav').onclick = function(event) {
     if(event.target.nodeName !=='SPAN') return;
+    closeAllSubMenu( event.target.nextElementSibling)
     event.target.nextElementSibling.classList.toggle('sub-menu-active')
+}
+
+function closeAllSubMenu(current = null) {
+    let parent = []
+    if(current) {
+        let currentParent = current.parentNode;
+        while(currentParent) {
+            if(currentParent.classList.contains('root-nav')) break;
+            if(currentParent.nodeName === 'UL') parent.push(currentParent);
+            currentParent = currentParent.parentNode;
+        }
+    }
+    const subMenu = document.querySelectorAll('.root-nav ul');
+    Array.from(subMenu).forEach(item => {
+
+        if(item !== current && !parent.includes(item))  item.classList.remove('sub-menu-active')
+    })
 }
 
